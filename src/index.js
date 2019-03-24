d5daa80f20d9886c3ca78f97fcd552d0b1a12856
JennyBortnouskaya <gienie.b17@gmail.com>
2019-03-24 23:06:33 +0300

    added:lexical changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let countLove = 0; 
+  preferences.forEach(function(element, index) { 
+  var love1 = preferences[element - 1]; 
+  var love2 = preferences[love1 - 1]; 
+  var love3 = preferences[love2 - 1]; 
   
-let result = 0;
-for (let i = 0; i < preferences.length; i++){
-  if (i + 1 === preferences[preferences[preferences[i] - 1] - 1]){
-    result++;
-  }
-}
-return Math.floor(result/3);
-};
+  if(element != love1 && love1 != love2 && love3 == element) { 
+  countLove++; 
+  preferences[element - 1] = undefined; 
+  preferences[love1 - 1] = undefined; 
+  preferences[love2 - 1] = undefined; 
+  } 
+  }) 
+  return countLove;
+  }; 
